d3789d3fea66805e2bdfb48eb003659b43a2644c
Patrick <torralbapatrick15@gmail.com>
2018-12-18 19:40:20 -0600

    Remove "../" on sw.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ if ('serviceWorker' in navigator) {
   // console.log('Service Worker Supported');
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw.js')
+      .register('sw.js')
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`))
   });
